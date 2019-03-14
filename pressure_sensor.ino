1304bfcfdd1615942418f90052055104094ccfe5
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2019-03-14 19:00:35 +0000

    Added ROS serial code, simplified code for initial tests

--- pressure_sensor.ino
+++ pressure_sensor.ino
@@ -1,13 +1,17 @@
+#include <ros.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 
-#define N_READINGS 100
+#define THRESHOLD 800
    
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-int cal_counter = 0;
 bool vacuum_flag;
-float baseline;
+
+ros::NodeHandle  nh;
+std_msgs::bool b_msg;
+ros::Publisher vacuum_on("vacuum_on", &b_msg);
+
 
 void setup(void) 
 {
@@ -18,36 +22,9 @@ void setup(void)
   {
     //If error, what to do? Send error message via ROS?
   }
-}
 
-void baseline_value()
-{
-  if (cal_counter = N_READINGS)
-  {
-    STATE = 1;
-    baseline = (pressure_sum / N_READINGS);
-  }
-  else
-    pressure_sum += event.pressure;
-}
-
-void empty_state()
-{
-  if (counter == 10)
-    vacuum_flag = true;
-    STATE = 2;
-
-  if (event.pressure > baseline + THRESHOLD )
-    counter++;
-}
-
-void holding_state()
-{
-  if (counter == 10)
-    vacuum_flag = false;
-
-  if (event.pressure < baseline - THRESHOLD)
-    counter++;
+  nh.initNode();
+  nh.advertise(vacuum_on);
 }
 
 void loop(void) 
@@ -56,22 +33,18 @@ void loop(void)
   sensors_event_t event;
   bmp.getEvent(&event);
 
-  switch(STATE)
+  if (event.pressure > THRESHOLD)
   {
-    case 0: 
-      baseline_value();
-      break;
-    
-    case 1:
-      empty_state();
-      break;
-    
-    case 2:
-      holding_state();
-      break;
+    vacuum_flag = true;
+  }
+  else if (event.pressure < THRESHOLD)
+  {
+    vacuum_flag = false;
   }
   
-  Serial.println(vacuum_flag);
+  str_msg.data = vacuum_flag;
+  vacuum_on.publish(&b_msg );
+  nh.spinOnce();
   
-  delay(10);
+  delay(100);
 }
