6c490b718a39145d18448b7bf3e1e2c2de3acd37
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2019-03-15 14:35:41 +0000

    Pressure sensor and vacuum toggle, working code

--- pressure_sensor.ino
+++ pressure_sensor.ino
@@ -2,49 +2,80 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 
-#define THRESHOLD 800
-   
+#define THRESHOLD 870
+#define N_READINGS 10
+#define VACUUM_PIN 12
+
+
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-bool vacuum_flag;
+bool vacuum_flag = false;
+long pressure_sum = 0;
+int pressure_avg;
+int counter = 0;
 
 ros::NodeHandle  nh;
-std_msgs::bool b_msg;
+std_msgs::Bool b_msg;
 ros::Publisher vacuum_on("vacuum_on", &b_msg);
 
 
-void setup(void) 
+void messageCb( const std_msgs::Empty& toggle_msg)
 {
-  Serial.begin(9600);
-  
+  if (digitalRead(VACUUM_PIN) == HIGH)
+    digitalWrite(VACUUM_PIN, LOW);
+  else
+    digitalWrite(VACUUM_PIN, HIGH);
+}
+
+ros::Subscriber<std_msgs::Empty> vacuum_toggle("toggle_vacuum", messageCb );
+
+
+
+void setup(void)
+{
+  //  Serial.begin(9600);
+
   /* Initialise the sensor */
-  if(!bmp.begin())
+  if (!bmp.begin())
   {
     //If error, what to do? Send error message via ROS?
   }
+  pinMode(VACUUM_PIN, OUTPUT);
 
   nh.initNode();
   nh.advertise(vacuum_on);
+  nh.subscribe(vacuum_toggle);
 }
 
-void loop(void) 
+void loop(void)
 {
-  /* Get a new sensor event */ 
+  /* Get a new sensor event */
   sensors_event_t event;
   bmp.getEvent(&event);
 
-  if (event.pressure > THRESHOLD)
+  if (counter == N_READINGS)
   {
-    vacuum_flag = true;
+    pressure_avg = (pressure_sum / N_READINGS);
+    pressure_sum = 0;
+    counter = 0;
+
+    if (pressure_avg < THRESHOLD)
+      vacuum_flag = true;
+
+    else if (pressure_avg > THRESHOLD)
+      vacuum_flag = false;
   }
-  else if (event.pressure < THRESHOLD)
+  else
   {
-    vacuum_flag = false;
+    pressure_sum += event.pressure;
+    counter++;
   }
-  
-  str_msg.data = vacuum_flag;
+
+  b_msg.data = vacuum_flag;
   vacuum_on.publish(&b_msg );
   nh.spinOnce();
-  
-  delay(100);
+
+  delay(10);
 }
